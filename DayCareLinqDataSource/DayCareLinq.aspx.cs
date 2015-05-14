736f86838e930b994902e2842b0d5f7267782be0
hromero-smartcare <hromero@smart.care>
2015-05-14 13:51:51 -0700

    Added daycare message queue handling

--- DayCareLinqDataSource/DayCareLinq.aspx.cs
+++ DayCareLinqDataSource/DayCareLinq.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using DayCareLinqDataSource.Models;
+using System.Messaging;
 
 namespace DayCareLinqDataSource
 {
@@ -31,31 +32,71 @@ namespace DayCareLinqDataSource
             var tDateCreated = ((TextBox)row.FindControl("DateCreated")).Text.Trim();
 
             DimPersonSCD1 person = new DimPersonSCD1();
+            person.PersonID = tGuid;
+            person.FirstName = tFirstName;
+            person.MiddleName = tMiddleName;
+            person.LastName = tLastName;
+            person.PreferredName = tPreferredName;
+            person.BirthDate = tBirthDate;
+            person.Gender = tGender;
+            person.Email = tEmail;
+            person.Ethnicity = tEthnicity;
+            person.DWUpdateDate = DateTime.Now;
+
+            //////// Create Message //////
+
+            System.Messaging.Message msg = new System.Messaging.Message();
+            msg.Body = person;
+
+            MessageQueue msgQin = new MessageQueue(".\\Private$\\daycare");
+            msgQin.Send(msg);
+
+            //////// Receive Message //////
+
+            MessageQueue msgQout = new MessageQueue(".\\Private$\\daycare");
+
+            DimPersonSCD1 qPerson = new DimPersonSCD1();
+            Object o = new Object();
+            System.Type[] arrTypes = new System.Type[2];
+            arrTypes[0] = qPerson.GetType();
+            arrTypes[1] = o.GetType();
+            msgQout.Formatter = new XmlMessageFormatter(arrTypes);
+            qPerson = (DimPersonSCD1)msgQout.Receive().Body;
+
+            ////////// Update Person object from db   ///////
+
+            DimPersonSCD1 newPerson = new DimPersonSCD1();
             using (var dbSmartReportingContext = new SmartReportingDevContext())
             {
-                person = dbSmartReportingContext.DimPersonSCD1.Where(p => p.PersonID == tGuid).FirstOrDefault<DimPersonSCD1>();
+                newPerson = dbSmartReportingContext.DimPersonSCD1.Where(p => p.PersonID == qPerson.PersonID).FirstOrDefault<DimPersonSCD1>();
             }
 
-            if (person != null)
+            if (newPerson != null)
             {
-                person.FirstName = tFirstName;
-                person.MiddleName = tMiddleName;
-                person.LastName = tLastName;
-                person.PreferredName = tPreferredName;
-                person.BirthDate = tBirthDate;
-                person.Gender = tGender;
-                person.Email = tEmail;
-                person.Ethnicity = tEthnicity;
-                person.DWUpdateDate = DateTime.Now;
+                newPerson.FirstName = qPerson.FirstName;
+                newPerson.MiddleName = qPerson.MiddleName;
+                newPerson.LastName = qPerson.LastName;
+                newPerson.PreferredName = qPerson.PreferredName;
+                newPerson.BirthDate = qPerson.BirthDate;
+                //newPerson.Gender = qPerson.Gender;        // Do not modify Ethnicity for now since lookup is not ready
+                newPerson.Email = qPerson.Email;
+                //newPerson.Ethnicity = qPerson.Ethnicity;  // Do not modify Gender for now since lookup is not ready
+                newPerson.DWUpdateDate = DateTime.Now;
+
+                /////// Update db with modified Person object //////////
 
                 using (var dbDestSmartReportingContext = new SmartReportingDevContext())
                 {
-                    dbDestSmartReportingContext.Entry(person).State = System.Data.Entity.EntityState.Modified;
+                    dbDestSmartReportingContext.Entry(newPerson).State = System.Data.Entity.EntityState.Modified;
                     dbDestSmartReportingContext.SaveChanges();
                 }
-            }
 
-           
+
+                // Cancel Update for now since hromero does not have permissions to make changes on DayCare_1
+                e.Cancel = true;
+
+
+            }
         }
     }
 }
